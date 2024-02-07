f1706811a36375881fb3a8e3e574162acc74097e
Daniels-123 <danielbuitragosarmiento@gmail.com>
2024-02-06 21:38:08 -0500

    fix proyect

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Daniel's Home Page",
+  title: "Fernando's Home Page",
   description: 'Generated with love by Vercel',
 }
 
